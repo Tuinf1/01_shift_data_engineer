54ba1100b0d50869e3326cac6816c1e934ffce61
Tuinf1 <cavabanga14741@mail.ru>
2025-07-21 17:40:42 +0700

    readme

--- init.sql
+++ init.sql
@@ -1,5 +1,6 @@
+--  создание таблицы: дата, метрики, значения, ключ
 CREATE TABLE weather_metrics (
-    date DATE NOT NULL,
+    date DATE NOT NULL,  
     metric TEXT NOT NULL,
     value TEXT,
     PRIMARY KEY (date, metric)
